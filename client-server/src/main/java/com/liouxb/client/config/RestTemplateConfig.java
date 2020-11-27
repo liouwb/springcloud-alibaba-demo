8ee315a56d3767468acc4c605fb621f7343e72b9
liouwb <liouxb@163.com>
2020-11-27 15:13:58 +0800

    1.provider-server集成sentinel做熔断和降级处理

--- client-server/src/main/java/com/liouxb/client/config/RestTemplateConfig.java
+++ client-server/src/main/java/com/liouxb/client/config/RestTemplateConfig.java
@@ -1,6 +1,5 @@
 package com.liouxb.client.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,14 +9,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @Configuration
 public class RestTemplateConfig {
-    /**
-     * ribbon负载均衡配置类IRule
-     * RoundRobinRule 默认的 轮询
-     * RandomRule随机
-     * WeightedResponseTimeRule 权重
-     *
-     * @return
-     */
+
     @Bean
     public RestTemplate restTemplate() {
 
--- provider-servce/src/main/java/com/liouxb/provider/controller/ProviderController.java
+++ provider-servce/src/main/java/com/liouxb/provider/controller/ProviderController.java
@@ -1,5 +1,6 @@
 package com.liouxb.provider.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +16,19 @@ public class ProviderController {
     private String serverPort;
 
     @GetMapping(value = "get")
+    @SentinelResource(value = "get", blockHandler = "exceptionHandler", fallback = "fallbackFun")// value 原方法 blockHandler  异常处理 fallback 降级处理
     public String get() {
 
         return "这是一个provider服务,服务端口:" + serverPort;
+    }
+
+    public String fallbackFun() {
+
+        return "@sentinel:" + serverPort;
+    }
+
+    public String exceptionHandler(){
+
+        return "@sentinel exceptionHandler:" + serverPort;
     }
 }
